b000f2374fdf099377068e14e0abd3426f96f934
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-08-27 19:12:33 +0800

    Implement basic arithmetic operations in Activity5.py

---
+++ Activity5.py
@@ -0,0 +1,39 @@
+#Operators
+# + = Addition
+# - = Subtraction
+# * = Multiplication
+# / = Division
+# ** = Exponentation
+# // = Floor Division
+# % = Modulus
+
+a = 8
+b = 13
+
+sum = a + b
+
+print("The sum of ",a," and",b," is ",sum)
+
+sum = b - a
+
+print("The sum of ",b," and",a," is ", sum)
+
+sum = a * b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a/b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a**b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a//b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a%b
+
+print("The sum of ",a," and",b," is ", sum) 
