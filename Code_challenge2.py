396415c4cfc97bac7d75da96eda19f01cfff438d
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-08-27 19:17:20 +0800

    Create Code_challenge2.py

---
+++ Code_challenge2.py
@@ -0,0 +1,24 @@
+#money to deposit
+money = 1201
+
+one_thousand = money//1000
+five_hundred = (money%1000)//500
+two_hundred = (money%500)//200
+one_hundred = (money%200)//100
+fifty = (money%100)//50
+twenty = (money%50)//20
+ten = (money%20)//10
+five = (money%10)//5
+one = (money%5)//1
+
+
+
+print("1000 = ",one_thousand)
+print("500 = ",five_hundred)
+print("200 = ",two_hundred)
+print("100 = ",one_hundred)
+print("50 = ",fifty)
+print("20 = ",twenty)
+print("10 = ",ten)
+print("5 = ",five)
+print("1 = ",one)
