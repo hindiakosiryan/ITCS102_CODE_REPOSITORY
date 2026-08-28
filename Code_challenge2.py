58c52ed51ed4451c473f683e375640e8c6064770
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-08-28 09:38:24 +0800

    Add currency denomination breakdown to Code_challenge2.py
    Calculate and display the number of each denomination in the account balance.

---
+++ Code_challenge2.py
@@ -0,0 +1,49 @@
+# Variables:
+Money = 1201
+a = 1000
+b = 500
+c = 200
+d = 100
+e = 50
+f = 20
+g = 10
+h = 5
+i = 1
+
+# Account balance
+print("Balance =", Money)
+
+# 1000
+print("1000 =", Money//a)
+
+# 500
+FiveH = Money%a
+print("500 =", FiveH//b)
+
+# 200
+TwoH = Money%b
+print("200 =", TwoH//c)
+
+# 100
+OneH = Money%c
+print("100 =", OneH//d)
+
+# 50
+Pipty = Money%d
+print("50 =", Pipty//e)
+
+# 20
+Binti = Money%e
+print("20 =", Binti//f)
+
+# 10
+Gis = Money%f
+print("10 =", Gis//g)
+
+# 5
+Lima = Money%g
+print("5 =", Lima//h)
+
+# 1
+isa = Money%h
+print("1 =", isa//i)
