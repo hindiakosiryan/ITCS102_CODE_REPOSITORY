065b19d0d0c002a858afc6541c87ec445bc60aa8
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-03 12:12:46 +0800

    Update activity6.py

--- activity6.py
+++ activity6.py
@@ -1,3 +1,18 @@
-name = input("what is your name?" )
+#name = input("what is your name?" )
 
-print("hi, ", name, ", how are you doing today?")
+#print("hi, ", name, ", how are you doing today?")
+
+
+#int()
+#eval()
+#float()
+#type()
+
+
+
+x = eval(input("Enter a number --> ?"))
+
+total = x + 5
+
+print(type(x))
+
