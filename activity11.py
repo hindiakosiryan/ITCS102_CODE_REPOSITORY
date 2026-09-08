ce2465317d5b6c6d94c380dfbd11a8fe3fcd935a
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-08 12:40:52 +0800

    Add username and password authentication

---
+++ activity11.py
@@ -0,0 +1,13 @@
+username = 'hindiakosiryan'
+password = 'akoposiryan143'
+
+u = input('Input Username ---> ')
+p = input('Input Password ---> ')
+
+
+if password != u and p != password : 
+        print("ACCESS DENIED")
+        print("MALIII wkwkwkw")
+else:
+        print("DONE")
+        print("WELCOME RYAN")
