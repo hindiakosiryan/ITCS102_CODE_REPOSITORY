d435f3e37ac7336b92bcc5fc972c318db05fbda7
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-08 12:40:25 +0800

    Create activity10.py

---
+++ activity10.py
@@ -0,0 +1,15 @@
+a = 123
+b = 6513
+c = 666
+d = 9547
+e = 123
+
+#aaaaaaaaaaaaaaaa
+
+print( a < c )
+print( a > d and c > e or e==a )
+print( e > c and c == e or e > a )
+
+#for not
+
+print( not( a > b or a == e and a < e ))
