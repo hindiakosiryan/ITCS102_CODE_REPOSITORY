71ccf9c1d9078e21159d045a7876d816f72a7c31
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-08 11:51:26 +0800

    Create activity9.py

---
+++ activity9.py
@@ -0,0 +1,25 @@
+x = 7
+y = 120
+text1 = 'Ryan'
+text2 = 'Wayan'
+
+#1 
+print( x > y )
+
+#2 
+print( x < y )
+
+#3 
+print( x >= y )
+
+#4 
+print( x <= y )
+
+#string for text
+
+#5
+print( text1 == text2 )
+
+#6
+print( text1 != text2 )
+
