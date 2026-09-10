ada52f169bfe44fb48a4d88d776784d673c823b7
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-10 11:42:15 +0800

    Fix login condition and update messages

--- activity11.py
+++ activity11.py
@@ -5,9 +5,9 @@ u = input('Input Username ---> ')
 p = input('Input Password ---> ')
 
 
-if password != u and p != password : 
+if password == u and p == password : 
         print("ACCESS DENIED")
-        print("MALIII wkwkwkw")
+       
 else:
         print("DONE")
-        print("WELCOME RYAN")
+       
