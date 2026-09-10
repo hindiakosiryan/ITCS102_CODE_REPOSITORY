4c1454c40b50ea4a1ce2a8e05bc7ced3f73ba248
hindiakosiryan <ryanbolanojr45@gmail.com>
2026-09-10 12:11:27 +0800

    Create activity13.py

---
+++ activity13.py
@@ -0,0 +1,29 @@
+#AGE DETERMINATION
+
+
+age = int(input("input age ---> ")) 
+
+if age >= 1 and age <= 5:
+       print("INFANT")
+
+if age >= 6 and age <= 12:
+       print("KID")
+
+if age >= 13 and age <= 19:
+       print("TEENAGER")
+
+if age >= 20 and age <= 29:
+       print("EARLY ADULT")
+
+if age >= 29 and age <= 45:
+       print("ADULT")
+
+if age >= 46 and age <= 59:
+       print("ADVANCE ADULTHOOD")
+
+if age >= 60 and age <= 150:
+       print("SENIOR")
+
+
+
+-
